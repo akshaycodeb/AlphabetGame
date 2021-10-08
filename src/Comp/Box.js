0255e7601c27665a076820bb6a454bc898e56648
akshaycodeb <akshay.pisal@codeb.online>
2021-10-08 11:34:47 +0530

    resize alphabets

--- src/Comp/Box.js
+++ src/Comp/Box.js
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { fabric } from 'fabric';
 
 const Box = () => {
-
-  // const [canvas, setCanvas] = useState('');
-
-  // useEffect(() => {
-  //   setCanvas(initCanvas());
-  // });
 
   useEffect(() => {
     initCanvas();
   });
 
-  const textCoardiantes = [{ text: "A", fixedLeft: 250, fixedTop: 400, dragLeft: 400, dragTop: 600, dragcolor: "yellow", fixedColor: "white" },
-  { text: "H", fixedLeft: 100, fixedTop: 400, dragLeft: 250, dragTop: 600, dragcolor: "red", fixedColor: "white" },
-  { text: "T", fixedLeft: 400, fixedTop: 400, dragLeft: 100, dragTop: 600, dragcolor: "grey", fixedColor: "white" }]
+  const textCoardiantes = [{ text: "A", fixedLeft: 250, fixedTop: 300, dragLeft: 400, dragTop: 500, dragcolor: "yellow", fixedColor: "white" },
+  { text: "H", fixedLeft: 100, fixedTop: 300, dragLeft: 250, dragTop: 500, dragcolor: "red", fixedColor: "white" },
+  { text: "T", fixedLeft: 400, fixedTop: 300, dragLeft: 100, dragTop: 500, dragcolor: "grey", fixedColor: "white" }]
 
   const initCanvas = () => {
 
     const Anime = new fabric.Canvas('canvas', {
-      height: 800,
+      height: 600,
       width: 600,
       backgroundColor: 'pink',
     })
@@ -31,11 +25,11 @@ const Box = () => {
     for (let index = 0; index < textCoardiantes.length; index++) {
       const element = textCoardiantes[index];
       let fabricTextFixed = new fabric.Text(element.text, {
-        fontSize: 100, textBackgroundColor: element.fixedColor, left: element.fixedLeft, top: element.fixedTop, lockMovementX: true,
+        fontSize: 80, textBackgroundColor: element.fixedColor, left: element.fixedLeft, top: element.fixedTop, lockMovementX: true,
         lockMovementY: true, selectable: false
       });
       let fabricTextDragable = new fabric.Text(element.text, {
-        fontSize: 100, textBackgroundColor: element.dragcolor, left: element.dragLeft, top: element.dragTop, hasControls: false,
+        fontSize: 80, textBackgroundColor: element.dragcolor, left: element.dragLeft, top: element.dragTop, hasControls: false,
       });
       Anime.add(fabricTextFixed)
       fixedObjects.push(fabricTextFixed)
@@ -120,52 +114,6 @@ const Box = () => {
       }
       )
     }
-
-    // function movingText3(e) {
-    //   Anime.on('mouse:up', function () {
-    //     if (parseInt(e.transform.target.left) > 50 && parseInt(e.transform.target.left) < 150 && parseInt(e.transform.target.top) < 450 && parseInt(e.transform.target.top) > 350) 
-    //     {
-    //       Text3.left = 100
-    //       Text3.top = 400
-    //       Text3.setCoords()
-    //       Text3.lockMovementX = true
-    //       Text3.lockMovementY = true
-    //       Text3.hasControls = false
-    //       Text3.selectable=false
-    //       Text2.hasControls = false
-    //       Text2.selectable=false
-    //     } else {
-    //       Text3.left = 250
-    //       Text3.top = 600
-    //       Text3.setCoords()
-    //     } })
-    //   Anime.renderAll()
-    // }
-
-    // function movingText4(e) {
-    //   Anime.on('mouse:up', function () {
-    //     if (parseInt(e.transform.target.left) > 200 && parseInt(e.transform.target.left) < 300 && parseInt(e.transform.target.top) < 450 && parseInt(e.transform.target.top) > 350) {
-    //       Text4.left = 250
-    //       Text4.top = 400
-    //       Text4.setCoords()
-    //       Text4.lockMovementX = true
-    //       Text4.lockMovementY = true
-    //       Text4.hasControls = false
-    //       Text4.selectable=false
-    //       Text.hasControls = false
-    //       Text.selectable=false
-    //     } else {
-    //       Text4.left = 400
-    //       Text4.top = 600
-    //       Text4.setCoords()
-    //     } })
-    //   Anime.renderAll()
-    // }
-
-    // Text5.on({ 'moving': moving });
-    // Text3.on({ 'moving': movingText3 });
-    // Text4.on({ 'moving': movingText4 });
-
   }
 
   return (
